85c6eefcfb87999c4360acc424d66add1be70d94
ameba23 <ameba23@systemli.org>
2019-10-12 10:28:29 +0200

    add generateZeroContribution

--- index.js
+++ index.js
@@ -17,7 +17,57 @@ exports.generateContribution = function (bls, ids, threshold) {
   // generate a sk and vvec
   for (let i = 0; i < threshold; i++) {
     const sk = new bls.SecretKey()
-    sk.setByCSPRNG(sk)
+    sk.setByCSPRNG()
+    svec.push(sk)
+
+    const pk = sk.getPublicKey()
+    vvec.push(pk)
+  }
+
+  // generate key shares
+  for (const id of ids) {
+    const sk = new bls.SecretKey()
+    sk.share(svec, id)
+    skContribution.push(sk)
+  }
+
+  svec.forEach(s => s.clear())
+
+  return {
+    verificationVector: vvec,
+    secretKeyContribution: skContribution
+  }
+}
+
+/**
+ * generates a members contribution to the DKG, ensuring the secret is null
+ * @param {Object} bls - an instance of [bls-lib](https://github.com/wanderer/bls-lib)
+ * @param {Array<Number>} ids - an array of pointers containing the ids of the members of the groups
+ * @param {Number} threshold - the threshold number of members needed to sign on a message to
+ * produce the groups signature
+ * @returns {Object} the object contains `verificationVector` which is an array of public key pointers
+ * and `secretKeyContribution` which is an array of secret key pointers
+ */
+exports.generateZeroContribution = function (bls, ids, threshold) {
+  // this id's verification vector
+  const vvec = []
+  // this id's secret keys
+  const svec = []
+  // this id's sk contributions shares
+  const skContribution = []
+
+  const zeroArray = Buffer.alloc(32)
+  const zeroSK = new bls.SecretKey()
+  zeroSK.deserialize(zeroArray)
+  svec.push(zeroSK)
+
+  const zeroPK = zeroSK.getPublicKey()
+  vvec.push(zeroPK)
+
+  // generate a sk and vvec
+  for (let i = 1; i < threshold; i++) {
+    const sk = new bls.SecretKey()
+    sk.setByCSPRNG()
     svec.push(sk)
 
     const pk = sk.getPublicKey()
@@ -41,7 +91,6 @@ exports.generateContribution = function (bls, ids, threshold) {
 
 /**
  * Adds secret key contribution together to produce a single secret key
- * @param {Object} bls - an instance of [bls-lib](https://github.com/wanderer/bls-lib)
  * @param {Array<Number>} secretKeyShares - an array of pointer to secret keys to add
  * @returns {Number} a pointer to the resulting secret key
  */
@@ -79,7 +128,6 @@ exports.verifyContributionShare = function (bls, id, contribution, vvec) {
 
 /**
  * Adds an array of verification vectors together to produce the groups verification vector
- * @param {Object} bls - an instance of [bls-lib](https://github.com/wanderer/bls-lib)
  * @param {Array<Array<Number>>} vvecs - an array containing all the groups verifciation vectors
  */
 exports.addVerificationVectors = function (vvecs) {
